203ee902c392caab30c8159936f71013afec1805
roshanjohnrj <151622861+roshanjohnrj@users.noreply.github.com>
2024-12-15 16:20:47 +0530

    Update vite.config.js

--- NewAdmin/vite.config.js
+++ NewAdmin/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
   proxy:{
-    '/api':'http://localhost:8000'
+    '/api':'https://smart-delivery-management-backend-2y57.onrender.com'
   }
   },
   plugins: [react()],
